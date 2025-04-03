26e734bae835fce1cd62c833a0dc7c0e19944ea7
domuk-k <dannyworks102@gmail.com>
2025-04-04 03:58:47 +0900

    chore: spring-mvc 초기화

---
+++ src/main/kotlin/edu/creamcommerce/CreamCommerceApplication.kt
@@ -0,0 +1,11 @@
+package edu.creamcommerce
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CreamCommerceApplication
+
+fun main(args: Array<String>) {
+    runApplication<CreamCommerceApplication>(*args)
+}
